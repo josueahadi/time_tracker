bc358348660f10feb956375acd00e0e724a625b1
Habib <habib@growrwanda.com>
2025-06-29 22:44:53 +0200

    feat: implement Project model with JSON serialization

---
+++ lib/models/project.dart
@@ -0,0 +1,19 @@
+class Project {
+  final String id;
+  final String name;
+  final bool isDefault;
+
+  Project({required this.id, required this.name, this.isDefault = false});
+
+  Map<String, dynamic> toJson() {
+    return {'id': id, 'name': name, 'isDefault': isDefault};
+  }
+
+  factory Project.fromJson(Map<String, dynamic> json) {
+    return Project(
+      id: json['id'],
+      name: json['name'],
+      isDefault: json['isDefault'] ?? false,
+    );
+  }
+}
